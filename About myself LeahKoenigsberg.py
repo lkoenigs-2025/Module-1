f7da1e91da8d11bc8ecc9f4894f3d7ba88420743
lkoenigs-2025 <lkoenigs@student.touro.edu>
2026-02-01 21:04:36 -0500

    Add files via upload

---
+++ About myself LeahKoenigsberg.py
@@ -0,0 +1,13 @@
+'''
+Hi, my name is Leah Koenigsberg.
+I am from Monsey, NY.
+
+I teach fourth grade girls in the morning, and complete college work in the afternoons.
+I am the youngest of a big family ka"h.
+In my free time, I enjoy hanging out with family and friends.
+
+In highschool, I used my creativity to design school newsletters and to put togeter videos.
+Similarly, in the summer as an assistant director of a large daycamp here in Monsey, I created thematic newsletters, flyers.
+
+My sister is a computer programmer who got her degree at Touro Flatbush and it seemed like a field I would also enjoy.
+'''
